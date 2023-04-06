8930c606ba011724122fb2ce362a182b6ab9c35a
gims-inc <gabrielmusanga@gmail.com>
2023-04-06 23:41:29 +0300

    triggers + sql arithmetic

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- script that creates a trigger that decreases the
+-- quantity of an item after adding a new order.
+DROP TRIGGER IF EXISTS update_qty;
+DELIMITER $$ ;
+CREATE TRIGGER update_qty
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE name = NEW.item_name
+END $$
+DELIMITER ; $$
