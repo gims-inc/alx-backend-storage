35c8cd80d00962845857a5feecac8f0503469926
gims-inc <gabrielmusanga@gmail.com>
2023-04-13 00:42:56 +0300

    reading from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
 Writing strings to Redis
+Reading from Redis and recovering original type 
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -23,9 +24,34 @@ class Cache:
 
         data: (Union)
         return:  key(str)
-
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
+        """Gets data from redis cache
+        """
+        data = self._redis.get(key)
+        if data and fn and callable(fn):
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Gets data as string from redis cache
+
+        key: str
+        return: str
+        """
+        data = self.get(key, lambda x: x.decode('utf-8'))
+        return data
+
+    def get_int(self, key: str) -> int:
+        """Gets data as integer from redis cache
+
+        key: str
+        Returns: data(int)
+        """
+        data = self
