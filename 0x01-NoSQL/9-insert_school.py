7969c38d8282379f4dcda1f3177835afabe48409
gims-inc <gabrielmusanga@gmail.com>
2023-04-12 03:40:08 +0300

    insert0ne

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Python function that inserts a new
+document in a collection based on kwargs
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """perform an insert in nosqldb
+    returns: _id
+    """
+    res = mongo_collection.insert_one(kwargs)
+    return res
